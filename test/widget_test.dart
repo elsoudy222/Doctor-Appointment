c7450b18233effe0efb9b72356a8cb6e98a9f9b0
soudyjr <elsoudy222@gmail.com>
2024-03-14 23:33:06 +0400

    refact code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,11 +10,12 @@ import 'package:doctor_appointment/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( DocApp(appRouter: AppRouter(),));
+    await tester.pumpWidget(DocApp(
+      appRouter: AppRouter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
