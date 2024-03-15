299126945495a33646ee5f5b313d656df7d86fb4
soudyjr <elsoudy222@gmail.com>
2024-03-15 23:42:08 +0400

    edite onboarding_screen.dart

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -5,6 +6,8 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Container(),
+    );
   }
 }
