94590f220727f21b0bd323f49121388e10d73c2d
soudyjr <elsoudy222@gmail.com>
2024-06-10 13:49:30 +0400

    complete Login Screen UI

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,13 +1,101 @@
+import 'package:doctor_appointment/core/theming/app_colors.dart';
+import 'package:doctor_appointment/core/widgets/app_text_form_field.dart';
+import 'package:doctor_appointment/features/login/ui/widgets/dont_have_account_text.dart';
+import 'package:doctor_appointment/features/login/ui/widgets/terms_and_conditions_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class LoginScreen extends StatelessWidget {
+import '../../../core/helper/spacing.dart';
+import '../../../core/theming/styles.dart';
+import '../../../core/widgets/app_text_button.dart';
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObscureText = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      AppTextFormField(
+                        hintText: "Email",
+                        validator: (String? value) {},
+                      ),
+                      verticalSpace(18),
+                      AppTextFormField(
+                        hintText: "Password",
+                        isObscureText: isObscureText,
+                        suffixIcon: GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              isObscureText = !isObscureText;
+                            });
+                          },
+                          child: Icon(
+                            color: ColorManager.mainBlue,
+                            isObscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+
+                          ),
+                        ),
+                        validator: (String? value) {},
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                      ),
+                      verticalSpace(40),
+                      AppTextButton(
+                        buttonText: "Login",
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {
+                         // validateThenDoLogin(context);
+                        },
+                      ),
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(60),
+                      const DontHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
